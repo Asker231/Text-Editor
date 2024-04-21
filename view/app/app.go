e4f42a659ec52e0c56a3a4b2521e7af86faa3712
Asker <kotsevasker0@gmail.com>
2024-04-21 12:16:53 +0300

    add func counter

--- view/app/app.go
+++ view/app/app.go
@@ -1,15 +1,25 @@
 package app
 
 import (
+
 	"texteditor/view/components/button"
+
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 )
 
 	func App(width float32 ,height float32){
+		// isTaped := false
 		 application := app.New()
 		 view := application.NewWindow("TextEditor")
 		 view.Resize(fyne.NewSize(width,height))
-         view.SetContent(button.Button(50,50))
+		 ct := container.NewWithoutLayout(button.Button(100,100))
+		 ct.Resize(fyne.NewSize(1200,1200))
+		 hstack := container.NewHBox(layout.NewSpacer(),ct,layout.NewSpacer())
+		 vstack := container.NewVBox(layout.NewSpacer(),hstack,layout.NewSpacer())
+		 view.SetContent(vstack)
 		 view.ShowAndRun()
 	}
--- view/components/button/button.go
+++ view/components/button/button.go
@@ -10,9 +10,18 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
+	func Counter()func()int{
+		var num int  = 0
+		return func()int{
+			 num+=1
+			return num
+		}
+	}
+
+
 	func Button(width float32,height float32) *widget.Button{
         icon,err := os.Open("/Users/asker/Desktop/Text-Editor/assets/Plus.png")
-
+        count := Counter() 
 		if err != nil{
 			fmt.Println(err)
 		}
@@ -26,7 +35,7 @@ import (
 		}
 
 		btn := widget.NewButtonWithIcon("", fyne.NewStaticResource("icon", b), func() {
-			// do something
+			fmt.Println(count())
 		})
 		btn.Resize(fyne.NewSize(width,height))
 		return btn
